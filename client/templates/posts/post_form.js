c1f7f6b0bed84c13c84486eb02f32e8719a892c8
Kelsie Yamakawa <kayama@hawaii.edu>
2015-10-28 22:26:25 -1000

    Finished WOD

---
+++ client/templates/posts/post_form.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Kelsie on 10/28/2015.
+ */
+
+Template.postForm.events({
+
+  // handle the form submission
+  'submit form': function(event, template) {
+
+    // stop the form from submitting
+    event.preventDefault();
+
+    Posts.insert({
+      title: event.target.title.value,
+      url: event.target.url.value
+    });
+
+    template.find("form").reset();
+  }
+});
